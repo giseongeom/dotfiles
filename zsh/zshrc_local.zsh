b947816827a1dd22b2e2819877fa25c6b1782242
GiSeong Eom <giseong.eom@krafton.com>
2022-06-04 00:11:40 +0900

    Update .zshrc_local.zsh

--- zsh/zshrc_local.zsh
+++ zsh/zshrc_local.zsh
@@ -1,2 +1,11 @@
 # zshrc_local.zsh
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+
+# ssh-agent
+if [ -z "$SSH_AUTH_SOCK" ]; then
+    eval `ssh-agent -s`  >/dev/null 2>&1
+    ssh-add >/dev/null 2>&1
+    ssh-add ~/.ssh/id_rsa_*pem >/dev/null 2>&1
+i
+
 
