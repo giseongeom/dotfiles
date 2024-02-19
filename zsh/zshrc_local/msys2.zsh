c7a8347560ee38be92f53040a46cb125517c6b4d
GiSeong Eom <giseong.eom@perfmon.io>
2024-02-19 13:27:27 +0900

    Add kubectl/helm/kubectx completion / msys2

--- zsh/zshrc_local/msys2.zsh
+++ zsh/zshrc_local/msys2.zsh
@@ -1,9 +1,38 @@
 # zshrc_local.zsh
 
+# kubectl autocompletion
+if [[ -x "$(command -v kubectl)" ]]; then source <(kubectl completion zsh); fi
+
+# helm autocompletion
+if [[ -x "$(command -v helm)" ]]; then source <(helm completion zsh); fi
+
+# yq autocompletion
+# https://stackoverflow.com/questions/71456694/not-sure-where-to-put-completions-with-oh-my-zsh
+if [[ -x "$(command -v yq)" ]]; then
+    yq shell-completion zsh > $ZSH_CACHE_DIR/completions/_yq
+fi
+
+# kubectx autocompletion
+if [[ -x "$(command -v kubectx)" ]] && [[ ! -e $ZSH_CACHE_DIR/completions/_kubectx.zsh ]]; then
+    cp -f /opt/kubectx/completion/_kubectx.zsh $ZSH_CACHE_DIR/completions/_kubectx.zsh
+fi
+
+# kubens autocompletion
+if [[ -x "$(command -v kubens)" ]] && [[ ! -e $ZSH_CACHE_DIR/completions/_kubens.zsh ]]; then
+    cp -f /opt/kubectx/completion/_kubens.zsh $ZSH_CACHE_DIR/completions/_kubens.zsh
+fi
+
+# eksctl autocompletion
+if [[ -x "$(command -v eksctl)" ]]; then
+    eksctl completion zsh > $ZSH_CACHE_DIR/completions/_eksctl
+fi
+
+
 # fastfetch
 if [[ -x "$(command -v fastfetch)" ]] && [[ $TERM_PROGRAM != "vscode" ]];
 then
     fastfetch --logo msys2
+    echo''
 fi
 
 # Perforce / $P4CONFIG
