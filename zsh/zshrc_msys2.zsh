876365d52ca5513f3b74816f84e8cba10b34b7cc
GiSeong Eom <giseong.eom@perfmon.io>
2024-02-19 14:40:44 +0900

    Fix ZSH location typo / msys2

--- zsh/zshrc_msys2.zsh
+++ zsh/zshrc_msys2.zsh
@@ -2,7 +2,7 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="/c/Users/giseong.eom/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
@@ -150,6 +150,13 @@ fi
 if [ -x "$(command -v aws_completer)" ]; then
     complete -C "$(command -v aws_completer)" aws
 fi
+
+# google-cloud-sdk
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/google-cloud-sdk/path.zsh.inc"; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/google-cloud-sdk/completion.zsh.inc"; fi
 
 
 # dotfile and so on
