f4e5630c851b3bfeccef274e0e62cc522390f496
GiSeong Eom <giseong.eom@perfmon.io>
2024-02-21 18:34:59 +0900

    Remove un-necessary stuff - msys2

--- zsh/rc/msys2.zsh
+++ zsh/rc/msys2.zsh
@@ -51,7 +51,7 @@ ZSH_THEME="agnoster2clock"
 # Uncomment the following line if you want to disable marking untracked files
 # under VCS as dirty. This makes repository status check for large repositories
 # much, much faster.
-DISABLE_UNTRACKED_FILES_DIRTY="true"
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
 
 # Uncomment the following line if you want to change the command execution time
 # stamp shown in the history command output.
@@ -73,7 +73,6 @@ plugins=(
     git
     zsh-syntax-highlighting
     zsh-autosuggestions
-    kube-ps1
 )
 
 source $ZSH/oh-my-zsh.sh
@@ -81,7 +80,7 @@ source $ZSH/oh-my-zsh.sh
 # User configuration
 
 # set PATH so it includes user's private bin directories
-PATH="$PATH:/c/Users/giseong.eom/bin:/c/Users/giseong.eom/scoop/shims:/c/Users/giseong.eom/AppData/Local/Programs/Microsoft VS Code/bin:/c//Program Files/Docker/Docker/resources/bin"
+PATH="$PATH:c/Users/giseong.eom/AppData/Local/Programs/Microsoft VS Code/bin:/c//Program Files/Docker/Docker/resources/bin"
 
 # $HOME/.local/bin
 if [ -d "$HOME/.local/bin" ] ; then
@@ -116,77 +115,6 @@ fi
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
-
-alias pbcopy="clip"
-alias k="kubectl"
-
-# kube-ps1
-if [[ $plugins == *kube-ps1* ]]; then
-    PROMPT=$PROMPT'$(kube_ps1) '
-    kubeoff
-fi
-
-# aws-cli autocompletion
-# https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-completion.html
-if [ -x "$(command -v aws_completer)" ]; then
-    complete -C "$(command -v aws_completer)" aws
-fi
-
-# google-cloud-sdk
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/google-cloud-sdk/path.zsh.inc"; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/google-cloud-sdk/completion.zsh.inc"; fi
-
-# kubectl autocompletion
-if [[ -x "$(command -v kubectl)" ]]; then source <(kubectl completion zsh); fi
-
-# helm autocompletion
-if [[ -x "$(command -v helm)" ]]; then source <(helm completion zsh); fi
-
-# yq autocompletion
-# https://stackoverflow.com/questions/71456694/not-sure-where-to-put-completions-with-oh-my-zsh
-if [[ -x "$(command -v yq)" ]]; then
-    yq shell-completion zsh > $ZSH_CACHE_DIR/completions/_yq
-fi
-
-# kubectx autocompletion
-if [[ -x "$(command -v kubectx)" ]] && [[ ! -e $ZSH_CACHE_DIR/completions/_kubectx.zsh ]]; then
-    cp -f /opt/kubectx/completion/_kubectx.zsh $ZSH_CACHE_DIR/completions/_kubectx.zsh
-fi
-
-# kubens autocompletion
-if [[ -x "$(command -v kubens)" ]] && [[ ! -e $ZSH_CACHE_DIR/completions/_kubens.zsh ]]; then
-    cp -f /opt/kubectx/completion/_kubens.zsh $ZSH_CACHE_DIR/completions/_kubens.zsh
-fi
-
-# eksctl autocompletion
-if [[ -x "$(command -v eksctl)" ]]; then
-    eksctl completion zsh > $ZSH_CACHE_DIR/completions/_eksctl
-fi
-
-# fastfetch
-if [[ -x "$(command -v fastfetch)" ]] && [[ $TERM_PROGRAM != "vscode" ]];
-then
-    fastfetch --logo msys2
-    echo''
-fi
-
-# Perforce / $P4CONFIG
-if [[ -f ${HOME}/.p4config ]];
-then
-    export P4CONFIG=${HOME}/.config/p4/p4config
-fi
-
-# Enable gsudo cache with zsh
-if [[ -x "$(command -v sudo)" ]] && [[ -f "/c/Users/giseong.eom/scoop/shims/sudo" ]];
-then
-    gsudon() {
-        my_zsh_pid=$(ps -p $$ | awk 'NR==2{print $4;exit}')
-        sudo cache on -p $my_zsh_pid
-    }
-fi
 
 # zsh
 autoload bashcompinit && bashcompinit
