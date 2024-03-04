7c43c654921d9ff41ef0328cfae634d82450f856
GiSeong Eom <giseong.eom@perfmon.io>
2024-03-05 01:26:40 +0900

    Rollback - zshrc

--- zsh/rc/git-bash.zsh
+++ zsh/rc/git-bash.zsh
@@ -121,20 +121,9 @@ then
     fastfetch -l CBL-Mariner
 fi
 
-# Lines configured by zsh-newuser-install
-HISTFILE=~/.histfile
-HISTSIZE=1000
-SAVEHIST=1000
-bindkey -e
-# End of lines configured by zsh-newuser-install
-# The following lines were added by compinstall
-#zstyle :compinstall filename '/c/Users/giseong.eom/.zshrc'
-
-autoload -Uz compinit
-compinit
-# End of lines added by compinstall
 
 
 # zsh
 autoload bashcompinit && bashcompinit
 autoload -Uz compinit && compinit
+
